856d5be194b4054681fee882bfed9398714b5391
linjenje-jr <salehelinjenje@gmail.com>
2026-09-22 18:55:42 +0300

    register fixed

--- register.py
+++ register.py
@@ -1,19 +1,20 @@
 #from bank2 import account_
-accounts=[]
+accounts={}
 def register():
     while True:
         global accounts
         account_name = input('enter your name ')
         account_password = input('enter your password ')
         print(f'account created for {account_name}')
-        accounts.append({account_name:account_password})
+        accounts[account_name] = account_password
         another_account = input('press1 to create another account or press 2 to log in\n')
         if another_account == '2':
             break
 
-def account():
-    balance = 0
-    name = input('enter your account name')
+register()
+#def account():
+balance = 0
+name = input('enter your account name')
 def password_verfication():
     global name
     global accounts
@@ -27,12 +28,10 @@ def password_verfication():
             if password == accounts[name]:
                 print(f'wellcome {name}, you have login your account')
                 break
-        print('access denide')
-        exit()
+            else:
+                print('access denide')
+                exit()
     
-
-register()
-account()
 password_verfication()
 
 
